4c508c882edac3f881a71b5cd8b8b2c1abb6fa5e
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2022-11-29 18:20:44 -0500

    Create script for password checking

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const password = document.querySelector('#pwd')
+const confirmPwd = document.querySelector('#confirm_pwd')
+const pwdMatch = document.querySelector('.pwd_no_match')
+const submitBtn = document.querySelector('button')
+
+
+
+
+function passwordMatch(){
+    if (password.value != confirmPwd.value){
+        pwdMatch.textContent = '* Password does not match';
+        password.style.outline = '0.5px solid red'
+        confirmPwd.style.outline = '0.5px solid red'
+        submitBtn.disabled = true
+    }
+    else if(password.value == '' || confirmPwd.value == ''){
+        pwdMatch.textContent == ''
+        submitBtn.disabled = true
+    }
+    else{
+        pwdMatch.textContent = '';
+        submitBtn.disabled = false
+    };
+}
+
+
+
+confirmPwd.addEventListener('keyup',passwordMatch)
+
+
